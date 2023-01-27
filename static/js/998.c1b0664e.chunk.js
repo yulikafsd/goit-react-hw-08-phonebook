1f534ced33669b13aa809a996af9ac03820b21fa
yulikafsd <ju.zagorovsky@gmail.com>
2023-01-27 19:35:55 +0000

    Deploying to gh-pages from @ yulikafsd/goit-react-hw-08-phonebook@36342739938c8c2335ba1c75bef9c6678d3e992b 🚀

--- static/js/998.c1b0664e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[998],{8998:function(e,t,o){o.r(t);var r=o(6804),h=o(3329);t.default=function(){return(0,h.jsxs)(r.x,{pt:3,pb:3,pr:4,pl:4,textAlign:"center",ml:"auto",mr:"auto",children:[(0,h.jsx)("h1",{children:"Homepage"}),(0,h.jsx)("h2",{children:"\ud83d\ude0e\ud83e\udd19\ud83d\udcf1"}),(0,h.jsx)("p",{children:"Welcome to the home page of your Phonebook"})]})}}}]);
-//# sourceMappingURL=998.c1b0664e.chunk.js.map
